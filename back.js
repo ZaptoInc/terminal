9f7129216738ebb78fc60f7a4dba234ecf22f036
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2023-06-17 13:17:10 +0200

    added ws support

--- back.js
+++ back.js
@@ -1,13 +1,40 @@
 const express = require('express')
+const ws = require('ws');
 const app = express()
 const port = 3000
+// prod/dev arg
+var env = process.env.NODE_ENV || 'dev';
 
+// serve files in public/
 app.use(express.static('public'))
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get('/env', (req, res) => {
+    res.send(env)
+})
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
+// websocket server
+
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+    console.log("[WS] new client connected");
+    socket.on('message', (data, isBinary) => {
+        var message = isBinary ? data : data.toString()
+        console.log(message)
+        socket.send(message);
+    });
+});
+
+// live reload on dev
+if (env == 'dev') {
+}
+
+// start express.js server
+const server = app.listen(port, () => {
+    console.log(`Env ${env} listening on port ${port}`)
+})
+
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+    });
+});
--- public/terminal.js
+++ public/terminal.js
@@ -1,6 +1,6 @@
 export const terminalManager = {
     commandHistory: [],
-    selectedCommand : -1,
+    selectedCommand: -1,
     currentCommand: "",
     typedCommand: "",
     path: "~",
@@ -11,19 +11,23 @@ export const terminalManager = {
         superUser: false
     },
 
+    socket: null,
+
+    socketEvents : {},
+
     addLog: function (log) {
         $("#logs").append('<p class="log">' + log + '</p>')
         // Scroll to the bottom
         window.scrollTo(0, document.body.scrollHeight);
     },
 
-    updateCurrentCommand: function() {
+    updateCurrentCommand: function () {
         var obj = {
-            username : this.user.name,
-            domain : this.domain,
-            path : this.path,
-            prefix : this.getPrefix(),
-            command : this.currentCommand
+            username: this.user.name,
+            domain: this.domain,
+            path: this.path,
+            prefix: this.getPrefix(),
+            command: this.currentCommand
         }
         var torender = $("#console").html()
         console.log(torender)
@@ -32,12 +36,46 @@ export const terminalManager = {
         $("#current-cmd").html(render)
     },
 
-    getPrefix: function() {
+    getPrefix: function () {
         if (this.user.superUser) {
             return "#\u00A0"
         } else {
             return "$\u00A0"
         }
-    }
+    },
+    sendToSocket: function (command, data) {
+        var message = JSON.stringify({
+            command,
+            data
+        })
+        this.socket.send(message)
+    },
 
+    initializeSocket: function (terminal) {
+        this.socket = new WebSocket(`ws://${document.location.host}`)
+        this.socket.addEventListener("open", function(event) {
+            terminal.sendToSocket("initialized", {test:true})
+        });
+        this.socket.addEventListener("close", function(event) {
+            setTimeout(function() { terminal.initializeSocket(terminal) }, 5000);
+        });
+        this.socket.addEventListener("message", function(event) {
+            var obj = JSON.parse(event.data)
+            if (obj.command && terminal.socketEvents[obj.command]) {
+                terminal.socketEvents[obj.command](obj)
+            }
+        });
+        this.setSocketEvent("initialized", function(test){
+            console.log(test)
+        })
+    },
+
+    setSocketEvent : function(command, func) {
+        this.socketEvents[command] = func
+    },
+
+    initialize: function () {
+        this.initializeSocket(this)     
+    }
 }
+
